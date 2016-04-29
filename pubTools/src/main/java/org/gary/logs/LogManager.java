6eca342e77241b6c79c9e06da1fad9e16c75848e
garyHuang <834865081@qq.com>
2016-04-29 13:16:24 +0800

    1

--- pubTools/src/main/java/org/gary/logs/LogManager.java
+++ pubTools/src/main/java/org/gary/logs/LogManager.java
@@ -3,13 +3,10 @@ package org.gary.logs;
 import org.apache.log4j.Logger;
 
 public class LogManager {
-
 	static Logger logger = Logger.getLogger(LogManager.class);
-
 	public static void info(Object msg) {
 		logger.info(msg);
 	}
-	
 	public static void err(String msg, Throwable e) {
 		
 		logger.error(msg, e);
