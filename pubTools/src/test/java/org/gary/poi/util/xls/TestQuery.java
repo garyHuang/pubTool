5cf11eeaa7eb5eee58f55ae807b3417333317190
garyHuang <834865081@qq.com>
2016-05-21 19:16:59 +0800

    xiugceshidaima

--- pubTools/src/test/java/org/gary/poi/util/xls/TestQuery.java
+++ pubTools/src/test/java/org/gary/poi/util/xls/TestQuery.java
@@ -1,6 +1,8 @@
 package org.gary.poi.util.xls;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.index.query.QueryBuilder;
@@ -19,14 +21,16 @@ public class TestQuery {
 		
 		//QueryBuilders.fuzzyQuery ("keyword", "制动")
 		
-		ElasticResponse query = searchUtils.query(0, 10 , SearchType.DFS_QUERY_THEN_FETCH , new QueryBuilder[]{
-				QueryBuilders.queryStringQuery("(30克)")
+		ElasticResponse query = searchUtils.query(0, 1554 , SearchType.DFS_QUERY_THEN_FETCH , new QueryBuilder[]{
+				QueryBuilders.queryStringQuery("0002-205\\/55R16")
 		}) ;
 		
-		
+		Set<Object> id2s = new HashSet<Object>();
 		for(Map<String,Object> data : query.getDatas()){
-			System.out.println( data );
+			id2s.add( data.get("partid"));
 		}
 		System.out.println( query.getTotlePage()); 
+		System.out.println("---------------" +id2s.size() );
+		
 	}
 }
