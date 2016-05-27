0f74e52c84370ee6564106aeab13e23623b47da3
garyHuang <834865081@qq.com>
2016-05-27 15:49:52 +0800

    修改 唯一加密 字符串

--- pubTools/src/main/java/org/gary/comm/utils/ShortUtils.java
+++ pubTools/src/main/java/org/gary/comm/utils/ShortUtils.java
@@ -10,6 +10,10 @@ public class ShortUtils {
 		System.out.println("短:" + shortUrl(sLongUrl, 8));
 	}
 
+	public static String shortUrl(String str){
+		return shortUrl( str , 6 ) ;
+	}
+	
 	public static String shortUrl(String url, int len) {
 		// 可以自定义生成 MD5 加密字符传前的混合 KEY
 		String key = "test";
@@ -17,9 +21,7 @@ public class ShortUtils {
 		String[] chars = new String[] { "a", "b", "c", "d", "e", "f", "g", "h",
 				"i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
 				"u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5",
-				"6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H",
-				"I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-				"U", "V", "W", "X", "Y", "Z" };
+				"6", "7", "8", "9" } ; 
 		// 对传入网址进行 MD5 加密
 		String hex = md5ByHex(key + url);
 
@@ -32,7 +34,7 @@ public class ShortUtils {
 		String outChars = "";
 		for (int j = 0; j < len; j++) {
 			// 把得到的值与 0x0000003D 进行位与运算，取得字符数组 chars 索引
-			long index = 0x0000003D & lHexLong;
+			long index = 0x00000023 & lHexLong;
 			// 把取得的字符相加
 			outChars += chars[(int) index];
 			// 每次循环按位右移 5 位
