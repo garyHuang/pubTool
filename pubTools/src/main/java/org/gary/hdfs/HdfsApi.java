b2643a139aeafc8a5d39a20afd36e1cf5ae2c5ad
834865081@qq.com <834865081@qq.com>
2016-06-08 17:15:45 +0800

    1

--- pubTools/src/main/java/org/gary/hdfs/HdfsApi.java
+++ pubTools/src/main/java/org/gary/hdfs/HdfsApi.java
@@ -1,23 +1,75 @@
 package org.gary.hdfs;
 
-import java.io.IOException;
+import java.io.OutputStream;
+ 
 
+ 
+
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
 import org.gary.logs.LogManager;
-
 
 
 public class HdfsApi {
 	
-	protected static FileSystem fs = null ;
-	
-	public void createFS(){
-		try {
-			fs = FileSystem.get(HadoopApi.getConfig()) ;
-		} catch (IOException e) {
-			LogManager.err( "创建fs出现错误" , e);
+	protected FileSystem hdfs = null;
+
+	public static class Holder {
+		
+		protected static HdfsApi hdfsApi = null;
+		protected static boolean isInstance = false ;
+		public static HdfsApi getHdfs(){
+			if(!isInstance){
+				hdfsApi = new HdfsApi();
+				try {
+					hdfsApi.hdfs = FileSystem.get(HadoopApi.getConfig()) ;
+				} catch (Exception e) { 
+				}
+			}
+			return hdfsApi ;
 		}
 	}
 	
 	
+	public static final HdfsApi getInstance() {
+		return Holder.getHdfs() ;
+	}
+	
+	public void put(String src , String dst){
+		try {
+			hdfs.copyFromLocalFile( true , new Path(src), new Path(dst)) ;
+			
+		} catch (Exception e) {
+			LogManager.err( "copy Form Local File error" , e );
+		}
+	}
+	
+	
+	
+	
+	public void mkdir(String dst){
+		try {
+			Path dstPath = new Path(dst) ;
+			if(hdfs.exists(dstPath)){
+				hdfs.mkdirs( dstPath ) ;
+			}
+		} catch (Exception e) {
+			LogManager.err( "mkdir" , e );
+		}
+	}
+	
+	/**
+	 * 从hdfs上拷贝文件
+	 * */
+	public void copyFileToOutput(String dst , OutputStream output){
+		FSDataInputStream input = null ;
+		try {
+			input = hdfs.open(new Path(dst));
+			IOUtils.copyBytes(input, output, 4096, true );
+		} catch (Exception e) {
+			LogManager.err( "copyFileToOutput" , e ); 
+		}
+	}
 }
